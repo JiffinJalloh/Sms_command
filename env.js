ed8e0da430d77bd3cc07f8e4c79e7d9fe2e195ac
JiffinJalloh <alhajimohamedj@yahoo.com>
2017-07-13 11:43:15 +0100

    update environment variables

--- env.js
+++ env.js
@@ -1,2 +1,13 @@
-process.env.ACCESS_TOKEN_KEY_NAME = 'admin'
-process.env.ACCESS_TOKEN_KEY_PASS = 'Password1'
+
+// module.exports ={
+// 	accessTokenKeyName: '',
+// 	accessTokenKeyPass: '',
+// 	baseUrl: '',
+// 	smsCommandUrl:''
+// }
+module.exports ={
+	accessTokenKeyName: 'admin',
+	accessTokenKeyPass: 'Password1',
+	baseUrl: 'https://dppi-idsr-sl.ehealthafrica.org/',
+	smsCommandUrl:'https://dppi-idsr-sl.ehealthafrica.org/dhis-web-maintenance-mobile/editSMSCommand.action?selectedCommandID=1'
+}
